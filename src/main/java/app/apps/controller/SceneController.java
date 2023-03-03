be6cc40f37a2a7749a397833cc1db742a853b940
Asandratra <asandratra.rakotondramaka@gmail.com>
2023-03-03 10:27:59 +0300

    Update SceneController
    Mbola tsy voatest fa misy probleme avec mon tomcat
    je mettrai une requete de pull et si ca ne devrais pas detruire le travail vous pouvez le merge

--- src/main/java/app/apps/controller/SceneController.java
+++ src/main/java/app/apps/controller/SceneController.java
@@ -1,5 +1,7 @@
 package app.apps.controller;
 
+import java.util.List;
+import java.sql.Time;
 import java.sql.Timestamp;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,9 @@ import javax.servlet.http.HttpSession;
 
 import app.apps.service.SceneService;
 import app.apps.service.PlanningService;
+import app.apps.service.FilmSetService;
+import app.apps.service.CharaterService;
+import app.apps.service.DialogueService;
 import app.apps.model.Scene;
 import app.apps.model.Planning;
 import app.apps.model.StatusPlanning;
@@ -24,16 +29,30 @@ import app.apps.model.Film;
 import app.apps.model.Gender;
 import app.apps.model.Actor;
 import app.apps.model.Dialogue;
+import app.apps.model.Character;
+import app.apps.model.Filmset;
 import app.apps.model.StatusPlanning;
 import app.apps.dao.HibernateDAO;
 
 @Controller
 public class SceneController {
     @Autowired
-    private SceneService ss;
+    HibernateDAO hibernate;
+
+    @Autowired
+    SceneService ss;
 
     @Autowired
     PlanningService ps;
+
+    @Autowired
+    FilmSetService fss;
+
+    @Autowired
+    CharaterService cs;
+
+    @Autowired
+    DialogueService ds;
 
     public static HttpSession session() {
         ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
@@ -132,16 +151,40 @@ public class SceneController {
 
     @GetMapping(value = "/film/{id}/scene/create")
     public String to_create(Model model) {
-        model.addAttribute("plateau", null);
-        model.addAttribute("liste_chara", null);
-        model.addAttribute("liste_character_json", null);
+        HttpSession session = null;
+        Film current = null;
+        session = SceneController.session();
+        current = (Film) session.getAttribute("current_film");
+        List<app.apps.model.Character> lc = cs.getCharacterByFilm(current.getId());
+        model.addAttribute("plateau", fss.getAllFilmSet());
+        model.addAttribute("liste_chara", lc);
+        model.addAttribute("liste_character_json", ps.listToJson(lc));
         return "create_scene";
     }
 
+    //titre,description,time_start,time_end,filmset,estimed_time
+    //dialogue_personnage[],dialogue_texte[],dialogue_action[]
     @PostMapping(value = "/film/{id}/scene/create")
-    public String create(Model model) {
+    public String create(String titre,String description,String time_start,String time_end,Integer filmset,String estimed_time,Integer[] d_perso,String[] d_dialogue,String[] d_action,Model model)throws Exception {
+        Scene s = null;
+        Dialogue d = null;
         try {
-            // ss.create();
+            s = new Scene();
+            s.setTitle(titre);
+            s.setGlobal_action(description);
+            s.setTime_start(Time.valueOf(time_start));
+            s.setTime_end(Time.valueOf(time_end));
+            s.setEstimated_time(Time.valueOf(estimed_time));
+            s.setFilmset((Filmset) hibernate.findById(Filmset.class,filmset));
+            s = ss.create(s);
+            d = new Dialogue();
+            d.setScene_id(s.getId());
+            for(int i=0;i<d_perso.length;i++){
+                d.setCharacter((app.apps.model.Character) hibernate.findById(app.apps.model.Character.class,d_perso[i]));
+                d.setTexte(d_dialogue[i]);
+                d.setAction(d_action[i]);
+                ds.createDialogue(d);
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
             throw ex;
